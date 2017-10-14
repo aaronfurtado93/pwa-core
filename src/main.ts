60df34e5ddd82cde5b6ff19ddaf275bcfec85245
Aaron Furtado <aaronfurtado93@hotmail.com>
2017-10-14 22:59:26 +0530

    implemented minimal configuration with service worker

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,8 @@ import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
+} else if (module[`hot`]) {
+  module[`hot`].accept();
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule)
