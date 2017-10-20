cb853d4173e98d40d26e55cadac993031ca836c7
Aaron Furtado <aaronfurtado93@hotmail.com>
2017-10-21 00:02:23 +0530

    allowed usage of JSON files as imports

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,5 +1,12 @@
 /* SystemJS module definition */
+// noinspection ES6ConvertVarToLetConst
 declare var module: NodeModule;
+// noinspection TsLint
 interface NodeModule {
   id: string;
+}
+
+declare module '*.json' {
+  const value: any;
+  export default value;
 }
